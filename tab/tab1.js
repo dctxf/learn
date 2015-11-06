cf015ccae9c1c1d3488288ca6a8659ee908cbdb4
dctxf <dctmz@qq.com>
2015-11-06 23:04:28 +0800

    mdified something

--- tab/tab1.js
+++ tab/tab1.js
@@ -4,12 +4,16 @@ $(function(){
         $cont = $tab.find('.cont').find('li'),
         i = 0;
 
-    for (; i < $btn.length; i++) !function (i) {
+    var clickFunc = !function (i) {
         // $btn[i].onclick = function(){
-        // 	$($cont[i]).show().siblings().hide();
+        //  $($cont[i]).show().siblings().hide();
         // };
         $($btn[i]).on('click', function () {
             $($cont[i]).show().siblings().hide();
         });
-    }(i);
+    };
+    
+    for (; i < $btn.length; i++) {
+        clickFunc(i);
+    }
 });
