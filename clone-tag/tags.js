1601332a1d3fa490f6a839bfe249d42d7bcff64f
dctxf <dctmz@qq.com>
2015-11-25 17:46:48 +0800

    clone-tag

--- clone-tag/tags.js
+++ clone-tag/tags.js
@@ -7,24 +7,34 @@ $(function(){
 
 	$tags.on('click','.tag',function(){
 		var text = $(this)[0].innerText,
-			$html = $('<li class="tag">'+text+'<span>X</span></li>');
+			$html = $('<li class="tag"><em>'+text+'</em><span>X</span></li>');
 		$(this).removeClass('tag').addClass('selected');
 		$html.appendTo($tagsCopy);
 	});
 
 	$tagsCopy.on('click','.tag',function(){
+		var text = $(this).find('em')[0].innerText,
+			tagText = $tags[0].innerText;
+			console.log(text);
 		$(this).remove();
 	});
 
 	$input.on('keydown',function(e){
-		if (e.which == 13) {
+		if (e.which == 13 && $input.val() !== '') {
 			var $html = $('<li class="tag">'+$(this).val()+'<span>X</span></li>'); 
 			$html.appendTo($tagsCopy);
-			$(this).attr('value','');
+			$input.val('');
 			return false;
-		}else{
-
+		}else if( $input.val() === '' && e.which == 8){
+			$tagsCopy.find('.tag:last-child').remove();
+		}else if (e.which == 13 && $input.val() === ''){
+			return false;
 		}
+	});
+
+	$('body').on('keydown',function(e){
+		// console.log(e.which);
+		// console.log();
 	});
 
 });
