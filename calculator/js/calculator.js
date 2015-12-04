bc75538388ce0e19dbb11e12531117352db51ec1
dctxf <dctmz@qq.com>
2015-12-04 17:15:59 +0800

    add

--- calculator/js/calculator.js
+++ calculator/js/calculator.js
@@ -9,6 +9,7 @@ window.onload = function(){
 		addBtn = document.getElementById('add'),
 		subtractBtn = document.getElementById('subtract'),
 		pointBtn = document.getElementById('point'),
+		minusBtn = document.getElementById('minus'),
 		result = 0;
 
 		resultShow(0);
@@ -47,7 +48,6 @@ window.onload = function(){
 		clearBtn.onclick = function(){
 			clear();
 		}
-			console.log(resultInputValue);
 
 		// 回退按钮
 		backBtn.onclick = function(){
@@ -66,28 +66,49 @@ window.onload = function(){
 
 		}
 
+		// 取反
+		minusBtn.onclick = function(){
+			minus();
+			console.log(result);
+			console.log(typeof result);
+		}
 		// 加法
 		addBtn.onclick = function(){
 			add();
-			return true;
+			this.Switch = true;
 		}
 
 		// 减法
 		subtractBtn.onclick = function(){
-
+			subtract();
+			this.Switch = true;
 		}
 
 		// 等于按钮
 		equalBtn.onclick = function(){
-			if (addBtn.onclick() === true) {
+			if (addBtn.Switch === true) {
 				add();
-				console.log(result);
+				addBtn.Switch = false;
+			}else if(subtractBtn.Switch === true){
+				subtract();
+				subtractBtn.Switch = false;
 			}
 		}
 
+		function minus(){
+			result = - resultInputValue;
+			resultInputValue = String(-parse(resultInputValue));
+			resultShow(result);
+		}
 		// 加法
 		function add(){
 			result += parse(resultInputValue);
+			resultInputValue = '0';
+			resultShow(result);
+		}
+		// 减法
+		function subtract(){
+			// result = result - parse(resultInputValue);
 			resultInputValue = '0';
 			resultShow(result);
 		}
