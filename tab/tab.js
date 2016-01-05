73d98fc077ff2331e96d48de668dc0fb18f9a4a8
dctxf <dctmz@qq.com>
2016-01-05 14:48:04 +0800

    整理

--- tab/tab.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	var $btn = $('#tab .btn li'),
-		$cont = $('#tab .cont li');
-
-	$btn.on('click',function(){
-		var index = $(this).index();
-		$cont.eq(index).show().siblings().hide();
-	});
-});
